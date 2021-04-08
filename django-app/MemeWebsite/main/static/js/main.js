1e8e22a544485479d064b84727107eb33ac6af01
Mohammed Abunada <m.abunada@msn.com>
2021-04-08 11:10:13 +0300

    prepped script for run & updated website

--- django-app/MemeWebsite/main/static/js/main.js
+++ django-app/MemeWebsite/main/static/js/main.js
@@ -16,35 +16,61 @@
 /* Grid */
 
 
-$(function() {
-    /**
-     * Store the transition end event names for convenience.
-     */
-    var transitionEnd =
-        "transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd";
-
-    /**
-     * Trigger the play button states upon clicking.
-     */
-    $(".play-btn").click(function(e) {
-        e.preventDefault();
-
-        if ($(this).hasClass("stop")) {
-            $(this).removeClass("stop").addClass("to-play");
-        } else if (!$(this).hasClass("to-play")) {
-            $(this).addClass("stop");
-        }
-    });
-
-    /**
-     * Remove the 'to-play' class upon transition end.
-     */
-    $(document).on(transitionEnd, ".to-play", function() {
-        $(this).removeClass("to-play");
-    });
-});
-
 const { useState } = React;
+
+
+
+var playing = false;
+var interval;
+
+function startplaying() {
+    var btn = $("#btn-2");
+    console.log(playing);
+    if (playing) {
+        playing = !playing;
+        clearInterval(interval);
+        btn.html(`<i class="fa fa-play"></i>`);
+    } else {
+        playing = !playing;
+        $("#current-date").html(document.getElementById("start_date").value)
+        get_mosaic(1)
+        interval = setInterval(get_mosaic, 15000, 1);
+        btn.html(`<i class="fa fa-pause"></i>`);
+    }
+
+}
+
+function get_mosaic(offset) {
+    document.getElementById("btn-1").disabled = true;
+    document.getElementById("btn-2").disabled = true;
+    document.getElementById("btn-3").disabled = true;
+    document.getElementById("subreddits").disabled = true;
+    var dt = document.getElementById("current-date").textContent;
+    var subreddit = document.getElementById("subreddits").value;
+    $("#app").html('<div class="text-center"><div class="spinner-border m-50" role="status" style="width: 15rem; height: 15rem;"><span class="sr-only">Loading...</span></div></div>')
+    $("#current-date").html('<div class="text-center"><div class="spinner-border m-0" role="status" style="width: 3rem; height: 3rem;"><span class="sr-only">Loading...</span></div></div>')
+    $.ajax({
+        type: "GET",
+        url: "/get_mosaic",
+        data: {
+            "subreddit": subreddit,
+            "date": dt,
+            "offset": offset,
+            'playing': playing,
+            'max_dt': document.getElementById("end_date").value,
+            'min_dt': document.getElementById("start_date").value
+        }
+    }).then(function(response) {
+        $("#app").html(response.body)
+        $("#current-date").html(response.dt)
+        document.getElementById("btn-1").disabled = false;
+        document.getElementById("btn-2").disabled = false;
+        document.getElementById("btn-3").disabled = false;
+        document.getElementById("subreddits").disabled = false;
+        document.getElementById("img_dwn").href = `/static/mosaics/${subreddit}_${response.dt.replaceAll("-", "")}.png`
+    });
+}
+get_mosaic(0)
 
 /*-----Developed by------|
 |------Mitch Chaiet------|
--- django-app/MemeWebsite/main/views.py
+++ django-app/MemeWebsite/main/views.py
@@ -14,17 +14,30 @@ def get_sql_connection():
 
 
 def main(request):
-    return render(request, 'main.html')
+    db = get_sql_connection()
+    options = list(pd.read_sql('select distinct(subreddit) from mosaics order by subreddit', db).iloc[:, 0])
+    db.dispose()
+
+    return render(request, 'main.html', context={'options':options})
 
 def get_mosaic(request):
     if request.is_ajax():
         subreddit = request.GET["subreddit"]
         date = pd.to_datetime(request.GET["date"])
+        print(request.GET["playing"])
+        
         offset = request.GET["offset"]
         
         date += timedelta(days=int(offset))
+        
+        max_date = pd.to_datetime(request.GET["max_dt"])
+        min_date = pd.to_datetime(request.GET["min_dt"])
+
+        if (bool(request.GET['playing'])) and date > max_date:
+            date = min_date
+        
         db = get_sql_connection()
-        df = pd.read_sql(f'select * from mosaics where subreddit="{subreddit}" and dt="{date.strftime("%y%m%d")}"', db)
+        df = pd.read_sql(f'select * from mosaics where subreddit="{subreddit}" and dt="{date.strftime("%y%m%d")}"', db).drop_duplicates()
         df['idx'] = df['x'] + (df['y'] * (df['x'].max()+1))
 
         df = df.sort_values('idx')
@@ -32,8 +45,8 @@ def get_mosaic(request):
         gridbody = '<div class="grid">'
 
         for _, row in df.iterrows():
-            gridbody += f'<a href="{row["full_link"]}" data-index="{row["idx"]}" class="item"><img style="width:100%; height:64" src="{row["url"]}"></img></a>'
+            gridbody += f'<a href="{row["full_link"]}" data-index="{row["idx"]}" class="item"><img style="width:100%; height:80" src="{row["url"]}"></img></a>'
         gridbody += "</div>"
         db.dispose()
-        
+
         return JsonResponse({"body":gridbody, "dt":pd.to_datetime(date).date().isoformat()})
